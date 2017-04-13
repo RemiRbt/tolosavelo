43398d80b48b0770abca9e6367b3451be7a0af2a
RemiRbt <remi.robichet@gmail.com>
2017-04-13 15:53:37 +0200

    GPS autorisation (failed/comment)

--- js/script.js
+++ js/script.js
@@ -2,11 +2,32 @@
 function renameStation(string) {
     var temp = "";
     // Si le nom de la station commence par "0", on supprimer les 8 premiers caractères (Voir données api JCdecaux)
-    if(string.charAt(0) == 0) {
+    if (string.charAt(0) == 0) {
         temp = string.substr(8);
-    // Sinon on supprimer les 6 premiers (Voir données api JCdecaux)
+        // Sinon on supprimer les 6 premiers (Voir données api JCdecaux)
     } else {
         temp = string.substr(6);
     }
     return temp;
 }
+
+
+/*$(document).ready(function () {
+    //Fonction plugin GPS
+
+    cordova.plugins.locationAccuracy.canRequest(function (canRequest) {
+        if (canRequest) {
+            cordova.plugins.locationAccuracy.request(function (success) {
+                console.log("Successfully requested accuracy: " + success.message);
+            }, function (error) {
+                console.error("Accuracy request failed: error code=" + error.code + "; error message=" + error.message);
+                if (error.code !== cordova.plugins.locationAccuracy.ERROR_USER_DISAGREED) {
+                    if (window.confirm("Failed to automatically set Location Mode to 'High Accuracy'. Would you like to switch to the Location Settings page and do this manually?")) {
+                        cordova.plugins.diagnostic.switchToLocationSettings();
+                    }
+                }
+            }, cordova.plugins.locationAccuracy.REQUEST_PRIORITY_HIGH_ACCURACY);
+        }
+    });
+
+});*/
